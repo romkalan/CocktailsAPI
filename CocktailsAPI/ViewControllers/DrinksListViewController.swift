3b17dd8ae0adafb68f6f22d61b4f6bc676af7167
romkalan <romkalan@mail.ru>
2023-03-23 17:01:43 +0300

    delete useless properties from model

--- CocktailsAPI/ViewControllers/DrinksListViewController.swift
+++ CocktailsAPI/ViewControllers/DrinksListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class DrinksListViewController: UITableViewController {
-
+    
     private let networkManager = NetworkManager.shared
     private var drinks = Drinks(drinks: [])
     
@@ -24,22 +24,28 @@ class DrinksListViewController: UITableViewController {
         
         detailDrinkVC.drink = drinks.drinks[indexPath.row]
     }
+}
 
 // MARK: - Table view data source
+extension DrinksListViewController {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         drinks.drinks.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "drink", for: indexPath)
         guard let cell = cell as? DrinkCell else { return UITableViewCell() }
         let drink = drinks.drinks[indexPath.row]
         cell.configure(with: drink)
-
+        
         return cell
     }
-    
+}
+
 // MARK: - Table view delegate
+extension DrinksListViewController {
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
